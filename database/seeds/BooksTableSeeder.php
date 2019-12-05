e1fc2de82f4d897be6681cae73a134e2829293cb
shionabe <pdpq310.s.a@gmail.com>
2019-12-05 22:15:20 +0900

    TEST 1 シーディングしたよ

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('books')->insert([
+        [
+          'book_name' => 'ブック１',
+        ],
+     ]);
+    }
+}
